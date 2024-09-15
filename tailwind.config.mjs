9f6dc82972243184a858d813271c05a08b81259c
Fansoa <fverin.contactpro@gmail.com>
2024-09-15 18:09:19 +0200

    adding custom fontFamily class in tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				text: 'Crimson Pro, Georgia, Times New Roman, Times, serif',
+				title: 'Oswald, Verdana, Helvetica, Arial, sans-serif' 
+			}
+		},
 	},
 	plugins: [],
 }
